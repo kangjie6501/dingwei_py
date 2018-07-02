d0c6588dc5b404db5c6ca7d69730600418e9821c
Kangjie <840416501@qq.com>
2018-07-02 20:04:17 +0800

    user

--- dingwei_app/models.py
+++ dingwei_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+    passWord = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
