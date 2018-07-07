0721ab9aa54f29bbfc11d8c7f45789a99db12fc4
kj <kangjie6501@qq.com>
2018-07-07 10:34:23 +0800

    定位后台

--- dingwei_app/models.py
+++ dingwei_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.CharField(max_length=32)
+    name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+# class Location(models.Model):
+#     jing = models.CharField(max_length=30)
+#     wei = models.CharField(max_length=30)
+#     date = models.CharField(max_length=30)
+#     id = models.CharField(max_length=32)
+#
+#     def __str__(self):
+#         return self.id
